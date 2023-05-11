c76927f00cf1a86c3d2b4f9b9029e59a945c7829
jeffersonmacedocarvalho <131269358+jeffersonmacedocarvalho@users.noreply.github.com>
2023-05-11 09:12:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,180 @@
+// export function calcular_area(num1,num2)
+// {
+//     const altura = num1
+//     const largura = num2
+//     const area = altura * largura
+    
+//     console.log(area);
+
+// }
+
+// const prim_num = Number(prompt("digite um numero:"))
+// const seg_num = Number(prompt("digite o segundo numero"))
+
+// calcular_area(prim_num,seg_num);
+
+//----------------------------------------------------------------
+
+// function funNome (nome,nom)
+// {
+// console.log(`olá ${nome}`);
+// }
+
+// funNome("felipe")
+// funNome("jefferson")
+// funNome("arthur")
+
+//----------------------------------------------------------------
+
+// function somar (num1,num2){
+//     const soma = num1 + num2
+//     return soma
+
+// }
+
+// const somatorio = somar(10,50)
+// console.log(somatorio);
+
+//------------------------------------------------------------------
+
+// const Numeros = [12,32,43,53]
+
+// function array(arrayNumeros){
+//     const num1 = arrayNumeros[0]
+//     const num2 = arrayNumeros[arrayNumeros.length -1]
+//     return [num1,num2]
+// }
+
+// const numFiltrados = array(Numeros)
+
+// console.log(numFiltrados);
+
+//------------------------------------------------------------------
+
+//função nao nomeada
+// const soma = function(n1,n2){
+//     return n1 + n2
+// }
+
+//------------------------------------------------------------------
+
+//arrow function
+// const somar = (n1, n2) =>{
+//     return n1 + n2
+// }
+
+//--------------------------------------------------------------------
+
+// function msg (){
+//     const mensagem = "eu sou jefferson macedo, tenho 18 anos, moro em são leopoldo e sou estudante"
+//     return mensagem
+// }
+
+// msg()
+// console.log(msg());
+
+//-------------------------------------------------------------------------------
+
+// const nome = prompt("informe seu nome")
+// const idade = Number(prompt(`informe sua idade ${nome}:`))
+// const cidade = prompt(`informe sua cidade ${nome}:`)
+// const profissao = prompt("informe sua profissão")
+
+// function mostrar(nome,idade,cidade,profissao)
+// {
+//     return (`olá ${nome}, a sua idade é:${idade} você mora na cidade:${cidade} e trabalha como:${profissao}`)
+// }
+
+// mostrar(nome,idade,cidade,profissao)
+// console.log(mostrar(nome,idade,cidade,profissao));
+
+//------------------------------------------------------------------------------------------
+
+// const num1 = Number(prompt("informe um numero"))
+// const num2 = Number(prompt("informe mais outro numero"))
+
+// function soma(num1,num2){
+//     const somar = num1 + num2
+//     return somar
+// }
+
+// soma(num1,num2)
+
+// console.log(`a soma destes numeros é:${soma(num1,num2)}`);
+
+//------------------------------------------------------------------------
+
+// const num1 = Number(prompt("informe um numero"))
+// const num2 = Number(prompt("informe mais outro numero"))
+
+// function soma(num1,num2){
+//     const somar = num1 + num2
+//     return somar
+// }
+
+// soma(num1,num2)
+
+// console.log(`a soma destes numeros é:${soma(num1,num2)}`);
+
+//------------------------------------------------------------------------------------
+
+// const n1 = Number(prompt("infrome um numero"))
+// const n2 = Number(prompt("informe outro numero"))
+
+// function somar (n1,n2){
+//     return n1 % n2 === 0
+// }
+
+//-----------------------------------------------------------------------------------
+
+// function mensagem(msg)
+// {
+//     const tamMsg = msg.length
+//     const msgMaiscula = msg.toUppercase()
+
+//     return [tamMsg , msgMaiscula]
+// }
+
+// console.log(mensagem("é na sola da bota, é na palma da bota"));
+
+//------------------------------------------------------------------------------------------
+
+// function somar(n1,n2){
+//     return n1 + n2
+// }
+
+// function subtrair(n1,n2){
+//     return n1 - n2
+// }
+
+// function multiplicar(n1,n2){
+//     return n1 * n2
+// }
+
+// function dividir(n1,n2){
+//     return n1 / n2
+// }
+
+// const n1 = Number(prompt("digite um numero"))
+// const n2 = Number(prompt("digite outro numero"))
+
+// console.log(`numeros inseridos ${n1} e ${n2}`);
+// console.log(somar(n1,n2));
+// console.log(subtrair(n1,n2));
+// console.log(multiplicar(n1,n2));
+// console.log(dividir(n1,n2));
+
+//------------------------------------------------------------------------------
+
+// const imprimir = (msg) => {
+//     console.log(msg);
+// }
+
+// const receber = (n1,n2) => {
+//     const somar = n1 + n2
+//     imprimir(somar)
+// }
+
+// receber()
+
+
